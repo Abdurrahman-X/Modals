184bb2d403d2e92a929bbfa57afdff3cd7bb2139
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-01-30 12:10:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const showModal = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');

b20bf8b252cd93be81fb993ec328e9fbb7a48a61
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-01-30 12:27:56 +0100

    selecting multiple classes

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const modal = document.querySelector('.modal');
 const showModal = document.querySelectorAll('.show-modal');
 const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
+
+for (let i = 0; i < showModal.length; i++) {
+    console.log(showModal[i].textContent); 
+}

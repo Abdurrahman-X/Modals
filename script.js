d05050391d5f619ed421a193a63dd7577fabd998
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-01-30 12:53:51 +0100

    showing the modal

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
 for (let i = 0; i < showModal.length; i++) {
-    console.log(showModal[i].textContent); 
+   showModal[i].addEventListener('click', function () {
+       console.log(showModal[i].textContent);
+       modal.classList.remove('hidden');
+       overlay.classList.remove('hidden');
+   })
 }

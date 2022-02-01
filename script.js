4119f64a08f8d38f9adbb6df0e3e9514ee831618
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-02-01 11:14:29 +0100

    added comments

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 'use strict';
 
+// SELECTORS    
 const modal = document.querySelector('.modal');
 const showModal = document.querySelectorAll('.show-modal');
 const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
 
-
+// Showing Modal
 for (let i = 0; i < showModal.length; i++) {
    showModal[i].addEventListener('click', function () {
        console.log(showModal[i].textContent);
@@ -15,8 +16,10 @@ for (let i = 0; i < showModal.length; i++) {
    })
 }
 
-
+// Closing Modal via "&times" button
 closeModalBtn.addEventListener('click', function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');   
 })
+
+// Closing Modal by clicking anywhere outside the Modal

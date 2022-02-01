e6db017d2b69208c63a1a850e10ad300746ab246
Abdurrahman Abolaji <abolajiabdurrahman@gmail.com>
2022-02-01 14:15:14 +0100

    refactored code

--- script.js
+++ script.js
@@ -6,20 +6,22 @@ const showModal = document.querySelectorAll('.show-modal');
 const closeModalBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
 
-
-// Showing Modal
-for (let i = 0; i < showModal.length; i++) {
-   showModal[i].addEventListener('click', function () {
-       console.log(showModal[i].textContent);
-       modal.classList.remove('hidden');
-       overlay.classList.remove('hidden');
-   })
+// created a openModal Function
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 }
 
 // created a closeModal Function
 const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
+}
+
+
+// Showing Modal
+for (let i = 0; i < showModal.length; i++) {
+   showModal[i].addEventListener('click', openModal)
 }
 
 // Closing Modal via "&times" button 
